8bec1577b0326a8c12fdffab9245efd165ed846d
JawsJust <68750752+JawsJust@users.noreply.github.com>
2021-06-29 12:35:47 -0500

    Update GUIs.java

--- jpizza/Libraries/GUIs.java
+++ jpizza/Libraries/GUIs.java
@@ -1,1 +1,24 @@
+package lemon.jpizza.Libraries;
 
+import lemon.jpizza.Objects.Executables.Library;
+import lemon.jpizza.Objects.Obj;
+import lemon.jpizza.Contextuals.Context;
+import lemon.jpizza.Results.RTResult;
+import lemon.jpizza.Objects.Primitives.*;
+
+@SuppressWarnings("unused")
+public class GUIs extends Library {
+
+    public GUIs(String name) { super(name); }
+
+    public RTResult execute_printDemo(Context execCtx) {
+        // Get value arg from variables passed into function
+        Obj value = (Obj) execCtx.symbolTable.get("value");
+        // Convert value argument to a string type
+        value = value.astring();
+        // Print value argument
+        System.out.println(value);
+        // Return null
+        return new RTResult().success(new Null());
+    }
+}
