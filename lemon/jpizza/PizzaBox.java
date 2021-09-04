df162dd1398174d6bde602a4bd44212f47549acc
Jack <jakitmationstudios@gmail.com>
2021-09-03 21:03:40 -0400

    Interpret method

--- lemon/jpizza/PizzaBox.java
+++ lemon/jpizza/PizzaBox.java
@@ -3,10 +3,11 @@ package lemon.jpizza;
 import lemon.jpizza.Nodes.Node;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class PizzaBox implements Serializable {
-    Node value;
-    public PizzaBox(Node value) {
+    List<Node> value;
+    public PizzaBox(List<Node> value) {
         this.value = value;
     }
 }
